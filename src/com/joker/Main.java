68857e2d420e8a9f65cf84b46448a7e66e103544
Jackie Chen <jackiechen2022@gmail.com>
2021-04-16 23:49:13 -0600

    Update Main

--- src/com/joker/Main.java
+++ src/com/joker/Main.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.IOException;
 
 public class Main {
-
     // High score
     public static int highScore = 0;
 
@@ -13,8 +12,6 @@ public class Main {
 
     // Create directories for the application
     public static final File dataDir = new File("memoryGameData/");
-
-    // TODO: High score file read/store
 
     public static void main(String[] args) throws IOException {
         dataDir.mkdir();
